2028dc21d6609b79ff168c76a950e85e8019a71a
Crosas27 <c.rosas27@icloud.com>
2026-03-05 16:42:38 -0700

    Update layoutEngine.js

--- js/core/layoutEngine.js
+++ js/core/layoutEngine.js
@@ -1,1 +1,19 @@
-a
+import { calculateRibs } from "./ribCalculator.js";
+import { calculatePanels } from "./panelCalculator.js";
+
+export function calculateLayout(wall) {
+
+  if (!wall) {
+    throw new Error("Wall object is required");
+  }
+
+  const ribs = calculateRibs(wall);
+
+  const panels = calculatePanels(wall, ribs);
+
+  return {
+    wall,
+    ribs,
+    panels
+  };
+}
