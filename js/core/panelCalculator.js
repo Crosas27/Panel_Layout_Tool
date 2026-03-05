3f489cfaa3e78fa27f6f7d74baba7b2f03aeb11f
Crosas27 <c.rosas27@icloud.com>
2026-03-05 16:40:44 -0700

    Update panelCalculator.js

--- js/core/panelCalculator.js
+++ js/core/panelCalculator.js
@@ -1,1 +1,21 @@
-a
+export function calculatePanels(wall, ribs) {
+
+  const panelCoverage = wall.panelCoverage;
+  const wallLength = wall.length;
+
+  const fullPanels = Math.floor(wallLength / panelCoverage);
+
+  const lastSeamPosition = fullPanels * panelCoverage;
+
+  const rippedPanelWidth = wallLength - lastSeamPosition;
+
+  const hasRippedPanel = rippedPanelWidth > 0;
+
+  return {
+    fullPanels,
+    rippedPanelWidth,
+    hasRippedPanel,
+    totalPanels: fullPanels + (hasRippedPanel ? 1 : 0),
+    lastSeamPosition
+  };
+}
