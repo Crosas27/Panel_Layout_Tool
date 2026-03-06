b0c87fa0cfe782eb4b75b72ffbf1846657a8d41d
Crosas27 <c.rosas27@icloud.com>
2026-03-05 22:57:06 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,28 +1,6 @@
-import { generateLayout } from "./core/layoutEngine.js"
-import { renderSvg } from "./renderer/svgRenderer.js"
 import { initUI } from "./ui/uiController.js"
 
 document.addEventListener("DOMContentLoaded", () => {
+ initUI();
+});
 
-  initUI()
-
-  const svgElement = document.getElementById("wallSvg")
-  const wallInput = document.getElementById("wallWidth")
-  const offsetInput = document.getElementById("offset")
-  const renderBtn = document.getElementById("renderBtn")
-
-  renderBtn.addEventListener("click", () => {
-
-    const wallLength = Number(wallInput.value) || 0
-    const offset = Number(offsetInput.value) || 0
-
-    const model = generateLayout({
-      wallLength,
-      offset
-    })
-
-    renderSvg(model)
-
-  })
-
-})
