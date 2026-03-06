ec4bae9a77cd382df36af0ccd9a1b97ca7ff9a53
Crosas27 <c.rosas27@icloud.com>
2026-03-05 21:24:34 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,28 @@
 import { calculateLayout } from "./core/layoutEngine.js"
-import { renderLayout } from "./renderer/svgRenderer.js"
+import { renderSvg } from "./renderer/svgRenderer.js"
 import { initUI } from "./ui/uiController.js"
 
 document.addEventListener("DOMContentLoaded", () => {
+
   initUI()
-  init()
-})
-
-function init() {
-
-  const renderBtn = document.getElementById("renderBtn")
-
-  renderBtn.addEventListener("click", handleRender)
-
-}
-
-function handleRender() {
 
   const svgElement = document.getElementById("wallSvg")
   const wallInput = document.getElementById("wallWidth")
   const offsetInput = document.getElementById("offset")
+  const renderBtn = document.getElementById("renderBtn")
 
-  const wallWidth = Number(wallInput.value) || 0
-  const offset = Number(offsetInput.value) || 0
+  renderBtn.addEventListener("click", () => {
 
-  const layout = calculateLayout({
-    wallWidth,
-    offset
+    const wallLength = Number(wallInput.value) || 0
+    const offset = Number(offsetInput.value) || 0
+
+    const model = calculateLayout({
+      wallLength,
+      offset
+    })
+
+    renderSvg(model)
+
   })
 
-  renderLayout(svgElement, layout)
-
-}
+})
